27abb08aa336a899ac02c0b15112c1755ec45d80
Rian Lucas <rianlucas2014@gmail.com>
2024-08-02 15:51:01 -0300

    feat: favourite country activity layout

---
+++ app/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.example.olympicagenda"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.olympicagenda"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/example/olympicagenda/ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.olympicagenda.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SelectFavouriteCountryActivity: AppCompatActivity(){
+}
